ffe66c63e52d2c330e4b8aac1dd5b09d1a739571
prplblck <kariurek@gmail.com>
2021-12-22 07:31:47 +0100

    working data read from JSON

--- script/jsonloader.js
+++ script/jsonloader.js
@@ -1,8 +1,22 @@
-let praktyki = {};
-fetch("/public/praktyki.json")
-    .then(response => response.json())
-    .then(json => {
-        Object.assign(praktyki,json)
-    });
+// let praktyki = {};
+// fetch("/public/praktyki.json")
+//     .then(response => response.json())
+//     .then(json => {
+//         Object.assign(praktyki,json)
+//     });
 
-    setTimeout(() => console.log(praktyki.praktyki[1]['tekst']), 1000)
+//     setTimeout(() => console.log(praktyki.praktyki[1]['tekst']), 1000)
+
+fetch("/public/praktyki.json")
+    .then(function(resp) {
+        return resp.json();
+    })
+    .then(function(data) {
+        console.log(data.praktyki);
+        console.log(Object.keys(data.praktyki).length);
+        const quotwrap = document.getElementById('qwrap');
+        for (let i = 0; i < Object.keys(data.praktyki).length; i++) {
+            console.log(data.praktyki[i]);
+
+        }
+    })
