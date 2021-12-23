83fd7979fc505004410b85a7550090e0249663d4
prplblck <kariurek@gmail.com>
2021-12-23 19:09:25 +0100

    random bg gradient

--- script/script.js
+++ script/script.js
@@ -5,4 +5,18 @@ function anim() {
     setTimeout(() => {
         nextbt.classList.toggle('anim');
     }, 300);
+    setBg();
 }
+
+
+
+const clrbl = document.querySelectorAll('.colorgrad');
+const setBg = () => {
+    // clrbl.style.backgroundColor = "#" + randomColor;
+    clrbl.forEach(element => {
+        const randomColor = Math.floor(Math.random()*16777215).toString(16);
+        element.style.backgroundColor = "#" + randomColor;
+        const randomRotation = Math.floor(Math.random()*360);
+        element.style.transform = "rotate(" + randomRotation +"deg)";
+    });
+  }
