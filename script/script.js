ee2735e3abc8eed1afea66215ac00bc64ec948eb
prplblck <kariurek@gmail.com>
2021-12-27 16:50:39 +0100

    fixed ios crash

--- script/script.js
+++ script/script.js
@@ -1,10 +1,6 @@
 const nextbt = document.getElementById('next')
 nextbt.addEventListener("click",anim)
 function anim() {
-    nextbt.classList.toggle('anim');    
-    setTimeout(() => {
-        nextbt.classList.toggle('anim');
-    }, 300);
     setBg();
 }
 
@@ -14,10 +10,14 @@ const clrbl = document.querySelectorAll('.colorgrad');
 const gradwrap = document.getElementById('gradbg');
 const setBg = () => {
     // clrbl.style.backgroundColor = "#" + randomColor;
-    clrbl.forEach(element => {
-        const randomColor = Math.floor(Math.random()*16777215).toString(16);
-        element.style.backgroundColor = "#" + randomColor;
-    });
-    const randomRotation = Math.floor(Math.random()*360);
-    gradwrap.style.transform = "rotate(" + randomRotation +"deg)";
+        let randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+        gradwrap.style.background = randomColor;
+        gradwrap.style.background = gradwrap.style.background.replace("/)","/,0.5/)");
+        let bgstyle = gradwrap.style.background;
+        bgstyle = bgstyle.replace("rgb","rgba");
+        bgstyle = bgstyle.replace("\)","\,0.2)")
+        gradwrap.style.background = bgstyle;
+        gradwrap.style.boxShadow = '#'+Math.floor(Math.random()*16777215).toString(16)+" 0vw -25vh 80vw -10vh inset";
+        console.log(bgstyle);
+        //const randomRotation = Math.floor(Math.random()*360);
   }
